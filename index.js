eadd59305e0a5a9919b725f71f62363cc0e1e337
Keith Garces <91123823+keith-garces@users.noreply.github.com>
2022-11-22 09:21:22 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let counter = 0
+
+const minusButton = document.getElementById("minus")
+const resetButton = document.getElementById("reset")
+const plusButton = document.getElementById("plus")
+const counterElement = document.getElementById("counter")
+
+minusButton.addEventListener("click", () => {
+    counter -= 1
+    counterElement.textContent = counter
+})
+
+resetButton.addEventListener("click", () => {
+    counter = 0
+    counterElement.textContent = counter;
+})
+
+plusButton.addEventListener("click", () => {
+    counter += 1
+    counterElement.textContent = counter
+})
